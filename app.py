8df59a365fb421868f412fd085fac979361b20cb
Nick Phillips <nickphillips8888@hotmail.co.uk>
2025-06-04 12:25:56 +0100

    Add open port for deployment

--- app.py
+++ app.py
@@ -122,4 +122,5 @@ def index():
     return render_template("index.html", result=answer_html, query=query, sources=sources)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=10000)
+
